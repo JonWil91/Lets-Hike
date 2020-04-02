0921add79e058015ab024af8c00b7aec13bb7d4e
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-04-02 16:14:28 +0000

    Added .gitignore and env.py files to keep mongoDB information secure

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = "hiking_database"
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'hello world'
+@app.route('/home_hikes')
+def home_hikes():
+    return render_template('index.html', hikes=mongo.db.hikes.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
