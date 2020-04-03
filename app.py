1e175eb265c10f97f8cceae936c3112cbee4010c
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-04-03 14:58:10 +0000

    Copied files from online tutorial for authentication feature

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+import bcrypt
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
@@ -17,6 +18,21 @@ mongo = PyMongo(app)
 @app.route('/home_hikes')
 def home_hikes():
     return render_template('index.html', hikes=mongo.db.hikes.find())
+
+@app.route('/about_me')
+def about_me():
+    return render_template('aboutme.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/login')
+def login():
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
+
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
