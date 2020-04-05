1ab9f39606639106fba990d3f75558aa6de96d0d
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-04-05 09:05:46 +0000

    Code for log in authentication commented out, to be revisited when site is fully functional

--- app.py
+++ app.py
@@ -10,14 +10,17 @@ if path.exists("env.py"):
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = "hiking_database"
+
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = 'mysecret'
+
 
 mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/home_hikes')
 def home_hikes():
-    return render_template('index.html', hikes=mongo.db.hikes.find())
+    return render_template('home.html', hikes=mongo.db.hikes.find())
 
 @app.route('/about_me')
 def about_me():
@@ -27,14 +30,9 @@ def about_me():
 def contact():
     return render_template('contact.html')
 
-@app.route('/login')
-def login():
-    if 'username' in session:
-        return 'You are logged in as ' + session['username']
-
-    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
     debug=True)
+    SECRET_KEY = os.environ.get('mysecret')
