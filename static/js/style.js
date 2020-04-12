f03e9430dfe5b453cdc7276fa7f501d91eb6de11
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-04-12 10:04:07 +0000

    Added js folder/file, added in leaflet map code copied from Code Institute tutor Tim template

---
+++ static/js/style.js
@@ -0,0 +1,9 @@
+var mapTileLayers = L.tileLayer("http://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
+    attribution: "Powered by <a href='https://developers.arcgis.com/terms/attribution/' target='_blank' rel='noopener'>Esri</a>"
+});
+
+var map = L.map("map", {
+    layers: [mapTileLayers],
+    center: [53.8008, 1.5491],
+    zoom: 5
+});
