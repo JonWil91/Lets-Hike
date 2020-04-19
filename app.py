0328fe78d302d72fc795ee3de1937f3e8c9c9548
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-04-19 16:40:32 +0000

    Removed county collection data from app.py

--- app.py
+++ app.py
@@ -29,29 +29,24 @@ def about():
 def hikes():
     _hikes = mongo.db.hikes.find()
     hike_list = [hike for hike in _hikes]
-    _counties = mongo.db.counties.find()
-    county_list = [county for county in _counties]
     _engcounties = mongo.db.eng_counties.find()
     eng_county_list = [engcounty for engcounty in _engcounties]
     _scotcounties = mongo.db.scot_counties.find()
     scot_county_list = [scotcounty for scotcounty in _scotcounties]
     _walescounties = mongo.db.wales_counties.find()
     wales_county_list = [walescounty for walescounty in _walescounties]
-    return render_template('hikes.html', hikes = hike_list,
-                                        counties = county_list, engcounties= eng_county_list,
+    return render_template('hikes.html', hikes = hike_list, engcounties= eng_county_list,
                                         scotcounties = scot_county_list, walescounties = wales_county_list)
 
 @app.route('/addhikes')
 def addhikes():
-    _counties = mongo.db.counties.find()
-    county_list = [county for county in _counties]
     _engcounties = mongo.db.eng_counties.find()
     eng_county_list = [engcounty for engcounty in _engcounties]
     _scotcounties = mongo.db.scot_counties.find()
     scot_county_list = [scotcounty for scotcounty in _scotcounties]
     _walescounties = mongo.db.wales_counties.find()
     wales_county_list = [walescounty for walescounty in _walescounties]
-    return render_template('addhikes.html', counties = county_list, engcounties= eng_county_list, scotcounties = scot_county_list,
+    return render_template('addhikes.html', engcounties= eng_county_list, scotcounties = scot_county_list,
                                             walescounties = wales_county_list)
 
 @app.route('/insert_hikes', methods=['POST'])
@@ -63,12 +58,10 @@ def insert_hikes():
 @app.route('/edit_hike/<hike_id>')
 def edit_hike(hike_id):
     the_hike =  mongo.db.hikes.find_one({"_id": ObjectId(hike_id)})
-    all_counties =  mongo.db.counties.find()
     eng_counties =  mongo.db.eng_counties.find()
     scot_counties =  mongo.db.scot_counties.find()
     wales_counties =  mongo.db.wales_counties.find()
-    return render_template('edithike.html', hike=the_hike,
-                            counties=all_counties, engcounties = eng_counties, 
+    return render_template('edithike.html', hike=the_hike, engcounties = eng_counties, 
                             scotcounties = scot_counties, walescounties = wales_counties)
 
 @app.route('/update_hike/<hike_id>', methods=["POST"])
