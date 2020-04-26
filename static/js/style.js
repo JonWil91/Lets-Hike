320180dbd7841986856e1ef7106ff9fb79fbfecc
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-04-26 11:48:30 +0000

    Removed customised markers from js file

--- static/js/style.js
+++ static/js/style.js
@@ -12,33 +12,7 @@ var map = L.map("map", {
 
 
 
-// Customised icons
 
-// let scotIcon = new L.Icon({
-//     iconUrl: "static/images/scotflag.png",
-//     iconSize: [25, 25],
-//     iconAnchor: [12, 25]
-// });
-
-// let engIcon = new L.Icon({
-//     iconUrl: "static/images/engflag.png",
-//     iconSize: [25, 25],
-//     iconAnchor: [12, 25]
-// });
-
-// let walesIcon = new L.Icon({
-//     iconUrl: "static/images/walesflag.png",
-//     iconSize: [25, 25],
-//     iconAnchor: [12, 25]
-// });
-
-// Markers for the three highest peaks in UK
-
-// const myScotMarker = L.marker([56.796956, -5.003272], { icon: scotIcon }).addTo(map).bindTooltip("Ben Nevis");
-
-// const myEnglandMarker = L.marker([54.595270, -3.195633], { icon: engIcon }).addTo(map).bindTooltip("Scarfell Pike");
-
-// const myWalesMarker = L.marker([53.073936, -4.076236], { icon: walesIcon }).addTo(map).bindTooltip("Snowden");
 
 
 
