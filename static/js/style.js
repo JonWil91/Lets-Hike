a962cda1d75a2763f94572cdb3567a073f056c95
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-05-05 10:51:54 +0000

    Moved style.js content to script tags in hikes.html

--- static/js/style.js
+++ static/js/style.js
@@ -1,17 +1,3 @@
-// Initialising LeafletJS Map
-
-var mapTileLayers = L.tileLayer("http://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
-    attribution: "Powered by <a href='https://developers.arcgis.com/terms/attribution/' target='_blank' rel='noopener'>Esri</a>"
-});
-
-var map = L.map("map", {
-    layers: [mapTileLayers],
-    center: [54.330606, -2.746197],
-    zoom: 5
-});
-
-
-
 
 
 
